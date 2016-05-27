ee4cdf2b392bf648fa8b737a81ee69dd1d8ade5f
orangehunter <te0919193534@gmail.com>
2016-05-27 16:03:58 +0800

    完成snackbar改寫

---
+++ app/src/main/java/com/exl/playrecord/Adapter/MySQLAdapter.java
@@ -0,0 +1,7 @@
+package com.exl.playrecord.Adapter;
+
+/**
+ * Created by michael on 2016/5/27.
+ */
+public class MySQLAdapter {
+}
--- app/src/main/java/com/exl/playrecord/MainActivity.java
+++ app/src/main/java/com/exl/playrecord/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    Boolean snackFlag=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        View view=fab.getRootView();
+
+
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Create Snackbar
+                    final Snackbar snackbar = Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_INDEFINITE);
+                    //Get Snackbar Layout
+                    Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
+                    //Hide textView
+                    TextView textView = (TextView) layout.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setVisibility(View.INVISIBLE);
+                    //Setup custom View
+                    LayoutInflater myInflater = getLayoutInflater();
+                    View snackView = myInflater.inflate(R.layout.my_snackbar, null);
+                    EditText editText = (EditText) snackView.findViewById(R.id.snackEditText);
+                    Button btn_add = (Button) snackView.findViewById(R.id.snackButtonAdd);
+                    View.OnClickListener clk_add = new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    };
+                    btn_add.setOnClickListener(clk_add);
+
+                    Button btn_cel = (Button) snackView.findViewById(R.id.snackButtonCancel);
+                    View.OnClickListener clk_cel = new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            snackFlag=false;
+                            snackbar.dismiss();
+                        }
+                    };
+                    btn_cel.setOnClickListener(clk_cel);
+
+                    layout.addView(snackView, 0);
+
+                    if (snackFlag==false) {
+                        snackFlag=true;
+                        snackbar.show();
+                    }
+                    /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();*/
+                }
+            });
+        }
     }
 
     @Override

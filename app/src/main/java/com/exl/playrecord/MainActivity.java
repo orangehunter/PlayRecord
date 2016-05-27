e35a8ee59c2c4c6180364a2f86ff8270703c5414
orangehunter <te0919193534@gmail.com>
2016-05-27 16:30:39 +0800

    snackbar改寫 取消cancel鈕

--- app/src/main/java/com/exl/playrecord/MainActivity.java
+++ app/src/main/java/com/exl/playrecord/MainActivity.java
@@ -1,8 +1,10 @@
 package com.exl.playrecord;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -18,6 +20,8 @@ import java.util.zip.Inflater;
 public class MainActivity extends AppCompatActivity {
 
     Boolean snackFlag=false;
+    Snackbar snackbar;
+    Snackbar.Callback call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,51 +32,45 @@ public class MainActivity extends AppCompatActivity {
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        View view=fab.getRootView();
-
 
         if (fab != null) {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    //Create Snackbar
-                    final Snackbar snackbar = Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_INDEFINITE);
-                    //Get Snackbar Layout
-                    Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
-                    //Hide textView
-                    TextView textView = (TextView) layout.findViewById(android.support.design.R.id.snackbar_text);
-                    textView.setVisibility(View.INVISIBLE);
-                    //Setup custom View
-                    LayoutInflater myInflater = getLayoutInflater();
-                    View snackView = myInflater.inflate(R.layout.my_snackbar, null);
-                    EditText editText = (EditText) snackView.findViewById(R.id.snackEditText);
-                    Button btn_add = (Button) snackView.findViewById(R.id.snackButtonAdd);
-                    View.OnClickListener clk_add = new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
+                    if (snackbar==null) {
+                        //Create Snackbar
+                        snackbar = Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_INDEFINITE);
 
-                        }
-                    };
-                    btn_add.setOnClickListener(clk_add);
+                        //Get Snackbar Layout
+                        Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
+                        layout.setBackgroundColor(ContextCompat.getColor(getBaseContext(),R.color.colorsnackEdit));
+                        //Hide textView
+                        TextView textView = (TextView) layout.findViewById(android.support.design.R.id.snackbar_text);
+                        textView.setVisibility(View.INVISIBLE);
+                        //Setup custom View
+                        LayoutInflater myInflater = getLayoutInflater();
+                        View snackView = myInflater.inflate(R.layout.my_snackbar, null);
+                        snackView.setBackgroundColor(ContextCompat.getColor(getBaseContext(),R.color.colorsnackEdit));
+                        EditText editText = (EditText) snackView.findViewById(R.id.snackEditText);
+                        Button btn_add = (Button) snackView.findViewById(R.id.snackButtonAdd);
+                        View.OnClickListener clk_add = new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                //TODO
+                            }
+                        };
+                        btn_add.setOnClickListener(clk_add);
 
-                    Button btn_cel = (Button) snackView.findViewById(R.id.snackButtonCancel);
-                    View.OnClickListener clk_cel = new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            snackFlag=false;
-                            snackbar.dismiss();
-                        }
-                    };
-                    btn_cel.setOnClickListener(clk_cel);
-
-                    layout.addView(snackView, 0);
+                        layout.addView(snackView, 0);
+                    }
 
                     if (snackFlag==false) {
                         snackFlag=true;
                         snackbar.show();
+                    }else {
+                        snackFlag = false;
+                        snackbar.dismiss();
                     }
-                    /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();*/
                 }
             });
         }

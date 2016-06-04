888f263d8aabc04dc2298a2089da1f5a6ffedbb3
orangehunter <te0919193534@gmail.com>
2016-06-04 13:12:05 +0800

    新增DB資料構造體

---
+++ app/src/main/java/com/exl/playrecord/Struct/DB_datas.java
@@ -0,0 +1,17 @@
+package com.exl.playrecord.Struct;
+
+/**
+ * Created by michael on 2016/6/4.
+ */
+public class DB_datas {
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_SEASON = "season";
+    public static final String KEY_EPISODE = "episode";
+    public static final String KEY_EPISODE_MAX = "episode_max";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_CREATE = "create_date";
+    public static final String KEY_UPDATE="update_date";
+    public static final String KEY_REMIND = "remind";
+}
